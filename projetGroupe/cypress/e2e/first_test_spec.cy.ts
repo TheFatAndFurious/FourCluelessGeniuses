911c018056e2ec40a626b67442b7afbf44350e05
EnelRahss <108793179+EnelRahss@users.noreply.github.com>
2024-01-11 17:03:50 +0100

    Update
    ajout de tests cypress

--- projetGroupe/cypress/e2e/first_test_spec.cy.ts
+++ projetGroupe/cypress/e2e/first_test_spec.cy.ts
@@ -8,12 +8,26 @@ describe('test_cypress', () => {
   beforeEach('visit',()=>{
     cy.visit('http://localhost:5173/');
   })
-  it('test de connexion ', () => {
-    cy.get('a').eq(2).click()
-    cy.get('input').first().type('fleury.sharlene@hotmail.fr')
-    cy.get('input').eq(1).type('RJ&malo80')
-    cy.get('button').eq(1).click()
-    cy.url()
-
+  // it('test de connexion et de déconnexion', () => {
+  //   cy.wait(100).get('input').first().wait(100).type('fleury.sharlene@hotmail.fr')
+  //   cy.wait(100).get('input').eq(1).type('RJ&malo80')
+  //   cy.wait(100).get('button').click()
+  //   cy.wait(1000).scrollTo(0, 500)
+  //   cy.get('button').click()
+  //   cy.wait(1000)
+  //   })
+    // it('navigation non connecté', () => {
+    //   cy.get('a').eq(1).click()
+    //   cy.wait(1000)
+    //   cy.get('a').eq(2).click()
+    //   cy.wait(1000)
+    //   cy.get('a').eq(0).click()
+    //   cy.wait(1000)
+    //   })
+    it('lien connexion', () => {
+    cy.wait(100).get('input').first().wait(100).type('fleury.sharlene@hotmail.fr')
+    cy.wait(100).get('input').eq(1).type('RJ&malo80')
+    cy.wait(100).get('button').click()
+      })
   })
-})
+
